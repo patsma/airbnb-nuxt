916adab387ad7788a59f4b0f080956dcf76ace13
Patryk Smakosz <patryksmakosz1@gmail.com>
2021-07-23 20:52:09 +0200

    Add base structure, config, dynamic home pages

---
+++ pages/home/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ home.title }}
+  </div>
+</template>
+<script>
+import homes from '~/data/home'
+
+export default {
+  data() {
+    return {
+      home: {}
+    }
+  },
+  created() {
+    // console.log(this.$route)
+    this.home = homes.find((home) => home.objectID === this.$route.params.id)
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectId" style="float: left;margin: 10px">
+      <home-card :home="home"/>
+    </div>
+  </div>
+</template>
+<script>
+import homes from '~/data/home.json'
+
+export default {
+  head() {
+    return {
+      title: 'Homepage',
+      meta: [{
+        name: 'description',
+        content: 'Thats a homepage',
+        hid:'description'
+      }]
+    }
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+    }
+  }
+}
+</script>
