061f67834fa57b4be272e7c875e8309a81770a21
Patryk Smakosz <patryksmakosz1@gmail.com>
2021-07-23 21:25:12 +0200

    Add base layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header style="background-color: #eee">
+      <NuxtLink to="/">Home</NuxtLink>
+    </header>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
