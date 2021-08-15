289b476b082f82a104f66c5ec90051cdc7cc2580
Patryk Smakosz <patryksmakosz1@gmail.com>
2021-08-15 12:11:56 +0200

    Add google maps and base algolia

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,21 @@
 export default {
-    components:true,
-    head:{
-        titleTemplate:'AirBnB | %s',
-        htmlAttrs:{
-            lang:'en'
-        },
-        bodyAttrs:{
-            class:['my-style']
-        },
-        meta:[{
-            charset:'utf-8'
-        }]
+  components: true,
+  head: {
+    titleTemplate: "AirBnB | %s",
+    htmlAttrs: {
+      lang: "en",
     },
-    router:{
-        prefetchLinks:false
-    }
-}
+    bodyAttrs: {
+      class: ["my-style"],
+    },
+    meta: [
+      {
+        charset: "utf-8",
+      },
+    ],
+  },
+  router: {
+    prefetchLinks: false,
+  },
+  plugins: ["~/plugins/maps.client", "~/plugins/dataApi"],
+};
--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -6,19 +6,23 @@
     </div>
     {{ home.title }}
     ${{ home.pricePerNight }} / night <br>
-    <img src="/images/marker.svg" width="20" height="20" alt=""/>{{ home.location.address }} {{ home.location.city }}{{ home.location.state }} {{ home.location.country }} <br>
+    <img alt="" height="20" src="/images/marker.svg" width="20"/>{{ home.location.address }} {{
+      home.location.city
+    }}{{ home.location.state }} {{ home.location.country }} <br>
     <img src="/images/star.svg" width="20" height="20" alt=""/>{{ home.reviewValue }}<br>
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath <br>
-
+    {{ home.description }}
+    <div ref="map" class="" style="height: 800px;width: 800px"></div>
   </div>
 </template>
 <script>
 import homes from '~/data/home'
 
 export default {
-  head(){
-    return{
-      title:this.home.title
+  head() {
+    return {
+      title: this.home.title,
+
     }
   },
   data() {
@@ -26,9 +30,15 @@ export default {
       home: {}
     }
   },
-  created() {
+  methods: {},
+  mounted() {
+    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
+  },
+  async created() {
     // console.log(this.$route)
     this.home = homes.find((home) => home.objectID === this.$route.params.id)
+    const homeData = await this.$dataApi.getHome(this.$route.params.id)
+    console.log(homeData)
   }
 }
 </script>
