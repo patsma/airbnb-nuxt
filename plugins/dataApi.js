c8ec0aae7ed1d025a41887b72f6bae86ad56e2eb
Patryk Smakosz <patryksmakosz1@gmail.com>
2021-09-26 02:07:26 +0200

    feature: algolia response basic bug handling

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,23 +1,45 @@
-
 export default function (context, inject) {
   const appId = "EYHE1Y8SW9";
   const apiKey = "deb5eed87e05a025752e5d14b88ae64b";
+  const headers = {
+    "X-Algolia-API-Key": apiKey,
+    "X-Algolia-Application-Id": appId,
+  };
 
   inject("dataApi", {
     getHome,
   });
 
   async function getHome(homeId) {
-    const response = await fetch(
-      `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
-      {
-        headers: {
-          "X-Algolia-API-Key": apiKey,
-          "X-Algolia-Application-Id": appId,
-        },
-      }
-    );
+    try {
+      return unWrap(
+        await fetch(
+          `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+          { headers }
+        )
+      );
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
+
+  async function unWrap(response) {
     const json = await response.json();
-    return json;
+    const { ok, status, statusText } = response;
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    };
+  }
+
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusMessage: error.message,
+      json: {},
+    };
   }
 }
