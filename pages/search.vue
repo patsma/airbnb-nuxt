1714f09a0e34576b9501b3bb40d9389a3755d74a
Patryk Smakosz <patryksmakosz1@gmail.com>
2021-09-30 02:37:20 +0200

    update: geo search page to refresh data on click, new data

--- pages/search.vue
+++ pages/search.vue
@@ -1,3 +1,23 @@
 <template>
-  <div> {{ $route.query }}</div>
+  <div class="">
+    <div> {{ lat }} / {{ lng }} / {{ label }}</div>
+    <div v-for="home in homes" :key="home.objectId">
+      {{ home.title }}<br>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  watchQuery: ['lat'],
+  async asyncData({query, $dataApi}) {
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+    return {
+      homes: data.json.hits,
+      label: query.label,
+      lat: query.lat,
+      lng: query.lng
+
+    }
+  }
+}
+</script>
